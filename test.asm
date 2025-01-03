08fc776706d8cbfc023bb285284c03695654f022
Book-reader <thevoid@outlook.co.nz>
2025-01-03 18:11:43 +0530

    partial rewrite

--- test.asm
+++ test.asm
@@ -1,10 +1,10 @@
-mov.64 r1, 0xff
-
+mov.64 r1, 0xff;
+# Comment
 loop:
-  mov.8, r0, [r1]
+  mov.8 r0, [r1];
   printc.8
-  add.64, r1, 1
-  cmp.8, [r0], 0
+  add.64 r1, 1;
+  cmp.8 [r0], 0
   jif 01, loop
 
 exit
