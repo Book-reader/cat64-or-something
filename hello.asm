e085c17a1d11ac39e96051b20bc21c939f5bbf08
Book-reader <thevoid@outlook.co.nz>
2025-02-08 21:06:25 +1300

    SO very WIP

--- hello.asm
+++ hello.asm
@@ -1,13 +1,19 @@
 # 88e8 00000000000000ff 01 8988 01 00 8006 88e4 0000000000000001 01 8582 00 00 9083 000000000000000b 800a
 # Prints "Hello, World!\n" assuming it is as 0xff in memory
-mov.64 r1, 0xff;
+# LDi.64 r1, 0xff;
+LDi r1, 0xff # Should be equivalent
 # Comment
-loop:
-  mov.8 r0, [r1];
-  printc.8
-  add.64 r1, 1;
-  cmp.8 r0, 0
-  jif 01, loop
-dbg
-exit
+$loop:
+  LD.8 r0, r1
+#  mov.8 r0, [r1];
+#  printc.8
+  PRINT.8 r0
+#  add.64 r1, 1;
+  ADDi.64 r1, 1
+#  cmp.8 r0, 0
+  CMPi.8 r0, 0
+#  jif 01, $loop
+  JNE $loop
+DBG
+EXIT
 # idx: db.8 0xff
