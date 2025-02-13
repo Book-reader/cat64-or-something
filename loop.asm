822b1115f9e1b281a6de2659033634c43ce70c56
Book-reader <thevoid@outlook.co.nz>
2025-02-14 09:36:36 +1300

    stuff

--- loop.asm
+++ loop.asm
@@ -1,20 +1,26 @@
 # 88e8 00000000000000ff 01 8988 01 00 8006 88e4 0000000000000001 01 8582 00 00 9083 000000000000000b 800a
 # Prints "Hello, World!\n" assuming it is as 0xff in memory
-mov.64 r1, 0xff;
-mov.64 r2, 100
-# Comment
-loop:
-  mov.8 r0, [r1];
-  printc.8
-  add.64 r1, 1;
-  cmp.8 r0, 0
-  jif 01, loop
-dbg
+# LDi.64 r1, 0xff;
+LDi r2, 10
+$start:
+	LDi r1, 0xff # Should be equivalent
+	#LDi r1, 0xff; NOP; LDi r1, 0xff # Semicolon test
+	# Comment
+	$loop:
+		LD.8 r0, r1
+		PRINT.8 r0
+	#	DBG
+		ADDi r1, 1
+	#	DBG
+		CMPi.8 r0, 0
+		JNE $loop
 
-# sub.64 r2, 1
-cmp.64 r3, 0
-#jmp 0
-jif 00, 12
+# DBG
+SUBi r2, 1
+# DBG
+CMPi r2, 0
+JNE $start
 
-exit
+DBG
+EXIT
 # idx: db.8 0xff
