b88973e61d97a55dcf25691241352cf58bba699c
Book-reader <thevoid@outlook.co.nz>
2025-02-19 11:10:09 +1300

    more stuff

--- print_int.asm
+++ print_int.asm
@@ -1,8 +1,10 @@
 @define input r0
+@define idx r2
 
 $print_int:
 #	PUSH input
 #	PUSH r1
+#	PUSH r2
 	ICMPi input, 0
 	JGT $print_int_1
 	PRINTi 0x2d # '-'
@@ -14,12 +16,32 @@ $print_int_1:
 	DIVi input, 10
 	MODi r1, 10
 	ADDi r1, 0x30 # '0'
+	LDi r3, $data
+	ADDi r3, r2
+	SDi
 	PRINT r1
 	CMPi r0, 0
-	JNE $print_int
+	JNE $print_int_1
+	PRINTi 0x0a
+#	POP r2
 #	POP r1
 #	POP input
 #	RET
 	EXIT
 
+# Define a bunch of empty bytes :)
+$data:
+	NOP.8
+	NOP.8
+	NOP.8
+	NOP.8
+	NOP.8
+	NOP.8
+	NOP.8
+	NOP.8
+	NOP.8
+	NOP.8
+	NOP.8
+	NOP.8
+	NOP.8
 @undefine input
